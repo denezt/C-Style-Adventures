2406b066996909a8283ee705828b699dd881d52e
Richard L. Jackson <denezt@users.noreply.github.com>
2022-01-04 18:34:38 +0100

    Code Refactoring
    Fixing the indentation, and removing extra spaces.

--- charfinder.c
+++ charfinder.c
@@ -23,59 +23,51 @@ Created 24 April 2015
 
 #define	 ERRORMSG1	"Error: Missing or invalid parameter was entered!\n"
 
-
 static const int reps = 10;
 int exit_state = 0;
 
 void ascii_to_int( char set_input ){
-	printf("The Char: %c is %i.\n", set_input, set_input );
-	}
+  printf("The Char: %c is %i.\n", set_input, set_input );
+}
 
 int selector( int svar, int get_input ){
-	int _ret = 0;
-	switch(svar){
-		case 'i':
-			ascii_to_int(get_input);
-		break;
-		default:
-			printf("Error: Missing command parameter!\n");
-			_ret = 1;
-		break;
-	}
-
-	return _ret;
-	}
+  int _ret = 0;
+  switch(svar){
+    case 'i':
+    ascii_to_int(get_input);
+    break;
+    default:
+    printf("Error: Missing command parameter!\n");
+    _ret = 1;
+    break;
+  }  
+  return _ret;
+}
 
 void help_menu(){
-	printf("Character Finder\n");
-	printf("charfinder\t-i\t[ convert to integer ]\n");
-	printf("charfinder\t-h\t[ Show help menu ]\n");
+  printf("Character Finder\n");
+  printf("charfinder\t-i\t[ convert to integer ]\n");
+  printf("charfinder\t-h\t[ Show help menu ]\n");
+}
 
-
-	}
-
-int main(int argc, char *argv[]){
-
-	if ( argc > 2 ){
-		char 	_type = *++argv[1];
-		char 	_input = *argv[2];
-		selector( _type, _input );
-	} else if ( argc == 2 ){
-		char	option = *++argv[1];
-		//
-		switch ( option ){
-			case 'h':
-				help_menu();
-			break;
-			default:
-				printf("Error: Try 'h' to view options.\n");
-			break;
-			}
-
-	} else {
-		printf(ERRORMSG1);
-		exit_state = 1;
-	}
-
-	return exit_state;
+int main(int argc, char *argv[]){  
+  if ( argc > 2 ){
+    char 	_type = *++argv[1];
+    char 	_input = *argv[2];
+    selector(_type, _input);
+  } else if ( argc == 2 ){
+    char	option = *++argv[1];    
+    switch ( option ){
+      case 'h':
+        help_menu();
+      break;
+      default:
+        printf("Error: Try 'h' to view options.\n");
+      break;
+    }
+  } else {
+    printf(ERRORMSG1);
+    exit_state = 1;
+  }  
+  return exit_state;
 }
